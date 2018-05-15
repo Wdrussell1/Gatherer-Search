0d0e057cd66f8276402c9733084bf3920cba2c74
Casey Barrett <casey.blu.barrett@gmail.com>
2018-05-15 12:53:23 -0500

    Initial Commit

---
+++ Gatherer Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gatherer Search
+//
+//  Created by Casey Barrett on 5/15/18.
+//  Copyright © 2018 BarrettPCSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
